e55299ca7d3bfe1b5461ca8de38f649d0f30253e
vBoulez <victorboulez@gmail.com>
2017-08-07 15:55:24 +1200

    id removed

--- Model/AllStatus.php
+++ Model/AllStatus.php
@@ -13,11 +13,6 @@ class AllStatus implements AllStatusInterface
     public function get() {
 		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 		$options = $objectManager->create('Magento\Sales\Model\ResourceModel\Order\Status\Collection')->toOptionArray();
-		$id = array();
-		for($i=1;$i<=count($options);$i++){
-			$id[$i]=$i;
-		}
-		$options = array_map(function($arr1,$arr2){return $arr1 + ['id' => $arr2];}, $options, $id);
         return $options;
     }
 }
